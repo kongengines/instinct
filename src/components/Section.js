971916d46026d793faf098494a5a5fee9e5bc9d9
kongengines <101394003+kongengines@users.noreply.github.com>
2022-03-13 19:01:36 +0100

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,7 +14,6 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background-size: cover;
     background-color: orange;
     background-image: url('/images/model-s.jpg');
 `
